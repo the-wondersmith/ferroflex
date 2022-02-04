1b988b00717a72379e4f3ce7192121ac937067d5
Mark S <the@wondersmith.dev>
2022-02-04 11:21:11 -0500

    🎉 Initial (empty) implementations of required traits for gluesql `Store`

--- src/sql/store.rs
+++ src/sql/store.rs
@@ -1,53 +1,47 @@
-// // Implementation of GlueSQL's required `Store` and `StoreMut` traits for DataFlex table files
-//
-// // Third-Party Imports
-// use async_trait::async_trait;
-// use gluesql::core::data::{Schema};
-// use gluesql::core::result::Result as SqlResult;
-// // use gluesql::core::result::MutResult as MutSqlResult;
-// use gluesql::core::store::{RowIter, Store}; // StoreMut
-//
-//
-// // Crate-Level Imports
-// use crate::structs::database::DataFlexDB;
-// // use crate::structs::table::{DataFlexTable, Row};
-//
-// #[allow(unused_variables)]
-// #[async_trait(?Send)]
-// impl Store<()> for DataFlexDB {
-//     async fn fetch_schema(&self, table_name: &str) -> SqlResult<Option<Schema>> {
-//         todo!()
-//     }
-//
-//     async fn scan_data(&self, table_name: &str) -> SqlResult<RowIter<()>> {
-//         todo!()
-//     }
-// }
-//
-// #[allow(unused_variables)]
-// #[async_trait]
-// impl StoreMut<Row> for DataFlexDB {
-//     async fn insert_schema(self, schema: &Schema) -> MutSqlResult<Self, ()> {
-//         todo!()
-//     }
-//
-//     async fn delete_schema(self, table_name: &str) -> MutSqlResult<Self, ()> {
-//         todo!()
-//     }
-//
-//     async fn insert_data(self, table_name: &str, rows: Vec<SqlRow>) -> MutSqlResult<Self, ()> {
-//         todo!()
-//     }
-//
-//     async fn update_data(
-//         self,
-//         table_name: &str,
-//         rows: Vec<(DataFlexTable, SqlRow)>,
-//     ) -> MutSqlResult<Self, ()> {
-//         todo!()
-//     }
-//
-//     async fn delete_data(self, table_name: &str, keys: Vec<u32>) -> MutSqlResult<Self, ()> {
-//         todo!()
-//     }
-// }
+// Implementation of GlueSQL's required `Store` and `StoreMut` traits for DataFlex table files
+
+// Third-Party Imports
+use async_trait::async_trait;
+use gluesql::core::data::{Row, Schema};
+use gluesql::core::result::MutResult as MutSqlResult;
+use gluesql::core::result::Result as SqlResult;
+use gluesql::core::store::{RowIter, Store, StoreMut};
+
+// Crate-Level Imports
+use crate::structs::DataFlexDB;
+
+#[allow(unused_variables)]
+#[async_trait(?Send)]
+impl Store<()> for DataFlexDB {
+    async fn fetch_schema(&self, table_name: &str) -> SqlResult<Option<Schema>> {
+        todo!()
+    }
+
+    async fn scan_data(&self, table_name: &str) -> SqlResult<RowIter<()>> {
+        todo!()
+    }
+}
+
+#[allow(unused_variables)]
+#[async_trait(?Send)]
+impl StoreMut<Row> for DataFlexDB {
+    async fn insert_schema(self, schema: &Schema) -> MutSqlResult<Self, ()> {
+        todo!()
+    }
+
+    async fn delete_schema(self, table_name: &str) -> MutSqlResult<Self, ()> {
+        todo!()
+    }
+
+    async fn insert_data(self, table_name: &str, rows: Vec<Row>) -> MutSqlResult<Self, ()> {
+        todo!()
+    }
+
+    async fn update_data(self, table_name: &str, rows: Vec<(Row, Row)>) -> MutSqlResult<Self, ()> {
+        todo!()
+    }
+
+    async fn delete_data(self, table_name: &str, keys: Vec<Row>) -> MutSqlResult<Self, ()> {
+        todo!()
+    }
+}
